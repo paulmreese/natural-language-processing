522cf8f725b47b9ea4b86bd68c990d06e4fae1b5
paulmreese <paul.m.reese@gmail.com>
2020-01-09 20:24:29 -0500

    Correct result semantics

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -17,12 +17,28 @@ function handleSubmit(event) {
         res['categories'].forEach(function(c) {
             console.log(c);
             document.getElementById('results').innerHTML = '';
-            for (const i in c) {
+            /*for (const i in c) {
                 if (i != "links") {
                     document.getElementById('results').innerHTML += `<p>${i}:` +
                     ` ${c[i]}</p>`
                 }
-            }
+            }*/
+            //alternative to looping through all params, use desired params
+            //can remove results initialization as the result!
+            document.getElementById('results').innerHTML =
+                `<tbody id="results">
+                    <tr>
+                        <th scope="row">IAB Category:</th>
+                        <td>${c.label}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Confidence:</th>
+                        <td>${c.confident ? "High" : "Low"} \(${(c.score * 100)
+                            .toFixed(2)}\% Match\)
+                        </td>
+                    </tr>
+                </tbody>`
+            //TODO: Add other Aylien API calls to further analyze text
         });
     })
 }
