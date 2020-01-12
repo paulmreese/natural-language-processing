a74b03ff1a513bb0303a05d3777e33bfad345995
paulmreese <paul.m.reese@gmail.com>
2020-01-12 13:23:17 -0500

    Update CSS precedence

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,10 @@
 import { handleSubmit } from './js/formHandler'
+import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-import './styles/resets.scss'
+import './styles/results.scss'
 import frogLogo from './img/Mesmerizing-Frog-Logo.png'
 
 var siteLogo = document.getElementById('logo');
