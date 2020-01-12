6acf690252c5dff3da48612e127205236c105e8a
paulmreese <paul.m.reese@gmail.com>
2020-01-11 21:54:04 -0500

    Add logo

--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,10 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 import './styles/resets.scss'
+import frogLogo from './img/Mesmerizing-Frog-Logo.png'
+
+var siteLogo = document.getElementById('logo');
+siteLogo.src = frogLogo;
 
 console.log("CHANGE!!");
 
